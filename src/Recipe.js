76ddf37a77738ad75604134f2035148e827dcd86
MHassaanButt <hassaanbutt67@gmail.com>
2021-07-03 12:32:58 +0500

    Final web with mobile view

--- src/Recipe.js
+++ src/Recipe.js
@@ -3,13 +3,14 @@ import style from "./recipe.module.css";
 const Recipe = ({ title, calories, image, ingredients }) => {
   return (
     <div className={style.recipe}>
-      <h1>{title}</h1>
+      <h1 className={style.title}>{title}</h1>
       <ol>
+        <h3>Recipe: </h3>
         {ingredients.map((ingredient) => (
           <li>{ingredient.text}</li>
         ))}
       </ol>
-      <p>{calories}</p>
+      <h4>Calories: {calories}</h4>
       <img className={style.image} src={image} alt="" />
     </div>
   );
